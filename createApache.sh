ec34da79d8b1a3b59d5b7aacadbe441bed6aa3b8
Fede Diaz <nordri@gmail.com>
2018-03-23 13:47:44 +0100

    update apache script

--- createApache.sh
+++ createApache.sh
@@ -3,8 +3,8 @@ set -e
 
 . config.rc
 
-sed -e "s/LDAP_SERVER/${LDAP_SERVER}/g" httpd-docker/vhost.conf.templ > httpd-docker/vhost.conf
-sed -i "s/LDAP_ROOT_DN/${LDAP_ACCOUNTBASE}/g" httpd-docker/vhost.conf
+sed -e "s/LDAP_SERVER/${LDAP_SERVER}/g" httpd/vhost.conf.templ > httpd/vhost.conf
+sed -i "s/LDAP_ROOT_DN/${LDAP_ACCOUNTBASE}/g" httpd/vhost.conf
 
 # Create Apache volume.
 docker volume create --name ${APACHE_VOLUME}
@@ -14,8 +14,8 @@ docker run  \
   --detach \
   --net ${CI_NETWORK} \
   -p 80:80 \
-  -v ${PWD}/httpd-docker/httpd.conf:/usr/local/apache2/conf/httpd.conf \
-  -v ${PWD}/httpd-docker/vhost.conf:/usr/local/apache2/conf/vhost.conf \
+  -v ${PWD}/httpd/httpd.conf:/usr/local/apache2/conf/httpd.conf \
+  -v ${PWD}/httpd/vhost.conf:/usr/local/apache2/conf/vhost.conf \
   -v ${APACHE_VOLUME}:/usr/local/apache2/www \
   httpd
 
