5990fa84ab5c441ad0c15c66d81adad0eea84ff9
gerrit developer <dev@example.com>
2018-04-19 15:22:40 +0200

    Using classic script call
    Change-Id: I684029bc90685ad456c7205fdedb909198bca1a8

--- Docker/docker-jenkins/entrypoint.sh
+++ Docker/docker-jenkins/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+
 echo "Genarate JENKINS SSH KEY"
-source /usr/local/bin/generate_key.sh
+/usr/local/bin/generate_key.sh
 echo "start JENKINS"
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
