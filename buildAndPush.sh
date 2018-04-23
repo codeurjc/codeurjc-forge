096df88476bf51715c58ff7d41fa39ba72d9b660
gerrit developer <dev@example.com>
2018-04-23 15:40:03 +0200

    Fix syntax error
    Change-Id: Ia8aa0924c659444f457bf5121be3682c36702261

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -8,24 +8,24 @@ cd Docker
 
 pushd docker-jenkins
 docker build -t codeurjc/forge-jenkins .
-docker tag codeurjc/forge-jenkins:latest
-docker tag codeurjc/forge-jenkins:${DATETIME}
+docker tag codeurjc/forge-jenkins codeurjc/forge-jenkins:latest
+docker tag codeurjc/forge-jenkins codeurjc/forge-jenkins:${DATETIME}
 docker push codeurjc/forge-jenkins:latest
 docker push codeurjc/forge-jenkins:${DATETIME}
 popd
 
 pushd gerrit-docker
 docker build -t codeurjc/forge-gerrit .
-docker tag codeurjc/forge-gerrit:latest
-docker tag codeurjc/forge-gerrit:${DATETIME}
+docker tag codeurjc/forge-gerrit codeurjc/forge-gerrit:latest
+docker tag codeurjc/forge-gerrit codeurjc/forge-gerrit:${DATETIME}
 docker push codeurjc/forge-gerrit:latest
 docker push codeurjc/forge-gerrit:${DATETIME}
 popd
 
 pushd ssp-docker
 docker build -t codeurjc/forge-php-ssp .
-docker tag codeurjc/forge-php-ssp:latest
-docker tag codeurjc/forge-php-ssp:${DATETIME}
+docker tag codeurjc/forge-php-ssp codeurjc/forge-php-ssp:latest
+docker tag codeurjc/forge-php-ssp codeurjc/forge-php-ssp:${DATETIME}
 docker push codeurjc/forge-php-ssp:latest
 docker push codeurjc/forge-php-ssp:${DATETIME}
 popd
