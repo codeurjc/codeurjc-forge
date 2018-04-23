61c956a4d71d1bd2f20610034710a50ee094df33
Fede Diaz <nordri@example.com>
2018-04-23 17:04:40 +0200

    addig commit tag instead of datetime
    Change-Id: Ife90b16e37f65427d7b228e55e6074bba910e860

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,33 +1,31 @@
 #!/bin/bash -x
 set -e
 
-DATETIME=$(date +"%Y%m%d%H%M%S")
-
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASS
 cd Docker
 
 pushd docker-jenkins
 docker build -t codeurjc/forge-jenkins .
 docker tag codeurjc/forge-jenkins codeurjc/forge-jenkins:latest
-docker tag codeurjc/forge-jenkins codeurjc/forge-jenkins:${DATETIME}
+docker tag codeurjc/forge-jenkins codeurjc/forge-jenkins:${TRAVIS_COMMIT}
 docker push codeurjc/forge-jenkins:latest
-docker push codeurjc/forge-jenkins:${DATETIME}
+docker push codeurjc/forge-jenkins:${TRAVIS_COMMIT}
 popd
 
 pushd gerrit-docker
 docker build -t codeurjc/forge-gerrit .
 docker tag codeurjc/forge-gerrit codeurjc/forge-gerrit:latest
-docker tag codeurjc/forge-gerrit codeurjc/forge-gerrit:${DATETIME}
+docker tag codeurjc/forge-gerrit codeurjc/forge-gerrit:${TRAVIS_COMMIT}
 docker push codeurjc/forge-gerrit:latest
-docker push codeurjc/forge-gerrit:${DATETIME}
+docker push codeurjc/forge-gerrit:${TRAVIS_COMMIT}
 popd
 
 pushd ssp-docker
 docker build -t codeurjc/forge-php-ssp .
 docker tag codeurjc/forge-php-ssp codeurjc/forge-php-ssp:latest
-docker tag codeurjc/forge-php-ssp codeurjc/forge-php-ssp:${DATETIME}
+docker tag codeurjc/forge-php-ssp codeurjc/forge-php-ssp:${TRAVIS_COMMIT}
 docker push codeurjc/forge-php-ssp:latest
-docker push codeurjc/forge-php-ssp:${DATETIME}
+docker push codeurjc/forge-php-ssp:${TRAVIS_COMMIT}
 popd
 
 docker logout
